6511c1fecca930cc2110552a5c6b421980307deb
Andy Li <qingyuanli23@mittymonarch.com>
2020-09-27 11:01:49 -0700

    add template

---
+++ probs/template.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//RUNNING:
+//g++ -O task.cpp
+
+int main() {
+    freopen("task.in", "r", stdin);
+    freopen("task.out", "w", stdout);
+    //use cin >> and cout <<
+
+    int n;
+    cin << n;
+}
