b543c9be00605455eba695431ce45802dd4049b6
Andy Li <qingyuanli23@mittymonarch.com>
2020-09-27 22:41:53 -0700

    binary tree

--- algos/tree/traversal.cpp
+++ algos/tree/traversal.cpp
@@ -4,14 +4,70 @@
 using namespace std;
 
 
-struct node
-{
-  int key_value;
-  node *left;
-  node *right;
+
+int arr[100];
+int N = 10;
+
+//binary tree code--------
+class Node {
+  public:
+  int val;
+  Node *left, *right;
 };
 
 
-int main() {
+Node * getBinaryTree(int i, int j) {
+  if(i <= j) {
+    int m = (i+j)/2;
+    Node* root = new Node();
+    root->val = arr[m];
+    root->left = getBinaryTree(i, m-1);
+    root->right = getBinaryTree(m+1, j);
 
+    return root;
+  }
+  return NULL;
+}
+
+//------------------
+
+
+//preorder---------------------------
+void preorder(Node* root) {
+  if(root != NULL) {
+    cout << root->val << " ";
+    preorder(root->left);
+    preorder(root->right);
+  }
+}
+
+//inorder----------------
+void inorder(Node* root) {
+  if(root != NULL) {
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+  }
+}
+
+//postorder----------------
+void postorder(Node* root) {
+  if(root != NULL) {
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val << " ";
+  }
+}
+
+//-------------------------------------
+
+
+int main() {
+  for(int i = 0; i < N; ++i) {
+    arr[i] = i;
+  }
+
+  Node* tree = getBinaryTree(0, N-1);
+
+  inorder(tree);
 }    
--- algos/tree/tree.cpp
+++ algos/tree/tree.cpp
@@ -1,6 +1,45 @@
-struct node
-{
-  int key_value;
-  node *left;
-  node *right;
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+//binary tree node--------
+class Node {
+  public:
+  int val;
+  Node *left, *right;
 };
+//------------------
+
+
+
+//create binary tree from array---------
+
+//NOTE: J SHOULD BE LEN(ARR)-1
+Node * getBinaryTree(int i, int j) {
+  if(i <= j) {
+    int m = (i+j)/2;
+    Node* root = new Node();
+    root->val = arr[m];
+    root->left = getBinaryTree(i, m-1);
+    root->right = getBinaryTree(m+1, j);
+
+    return root;
+  }
+  return NULL;
+}
+//-------------------------
+
+
+
+int main() {
+  //example usage
+  Node *tree = new Node();
+  tree->val = 5;
+  tree->left = new Node();
+  tree->left->val = 10;
+  
+  cout << tree->left->val << "\n";
+}
