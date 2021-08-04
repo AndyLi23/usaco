1cc860890746bce85774c5d45cce823f86099105
AndyLi23 <andyliqy@gmail.com>
2021-08-04 16:18:39 -0700

    segment trees

---
+++ algos/tree/segsum.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for(int (i) = 0 ; (i) < (n); ++(i))
+#define FOR2(i, a, b) for(int (i) = (a); (i) < (b); ++(i))
+#define FOR2R(i, b, a) for(int (i) = (b); (i) >= (a); --(i))
+#define ll long long
+
+int N = 8;
+int a[8]{5, 8, 6, 3, 2, 3, 2, 6};
+int tree[200000]; //size should be at least 2N
+
+//**** ALL PARAMETERS ARE 0-INDEXED *****
+//sum from range a to b
+int sum(int a, int b) {
+    a += N; b += N;
+    int s = 0;
+    while(a <= b) {
+        if(a%2==1) s += tree[a++];
+        if(b%2==0) s += tree[b--];
+        a /= 2; b /= 2;
+    }
+    return s;
+}
+
+//add x to element at index k
+void add(int k, int x) {
+    k += N;
+    tree[k] += x;
+    for (k /= 2; k >= 1; k/=2) {
+        tree[k] = tree[2 * k] + tree[2 * k + 1];
+    }
+}
+
+//initialize with another array a (size should be N)
+void init(int a[]) {
+    FOR2(i, N, 2*N) {
+        tree[i] = a[i - N];
+    }
+    FOR2R(i, N-1, 1) {
+        tree[i] = tree[2 * i] + tree[2 * i + 1];
+    }
+}
+
+int main() {
+    init(a);
+    add(5, 4);
+
+    FOR(i, 2*N) {
+        cout << tree[i] << " ";
+    }
+    cout << endl << sum(2,7);
+}
