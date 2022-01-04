33be6f5db2e36f9d376383067f7ca1d5651121be
AndyLi23 <andyliqy@gmail.com>
2022-01-04 13:47:57 -0800

    cowmbat

---
+++ probs/gold/december_2019/cowmbat.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, n) for(int (i) = 0 ; (i) < (n); ++(i))
+#define FOR2(i, a, b) for(int (i) = (a); (i) < (b); ++(i))
+#define FOR2R(i, b, a) for(int (i) = (b); (i) >= (a); --(i))
+#define ll long long
+#define pb push_back
+#define MOD 1000000007;
+const int MAXN = 1e5+5;
+
+int N, M, K, a[26][26], cost[MAXN][26], psum[MAXN][26];
+int dp[MAXN][26], dpmin[MAXN];
+string s;
+
+int main() {
+    freopen("cowmbat.in", "r", stdin);
+    freopen("cowmbat.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    //use cin >> and cout <<
+    //run local: g++ -std=c++11 -O2 -Wall task.cpp -o a / DONT USE UNORDERED MAP!!!!!
+
+    cin >> N >> M >> K;
+    cin >> s;
+
+    FOR(i, M) FOR(j, M) cin >> a[i][j];
+    FOR(k, M) FOR(i, M) FOR(j, M) a[i][j] = min(a[i][j], a[i][k] + a[k][j]);
+
+    FOR(i, N) {
+        FOR(j, M) {
+            cost[i][j] = a[s[i] - 'a'][j];
+            psum[i][j] = cost[i][j];
+        }
+    }
+
+    FOR2(i, 1, N) {
+        FOR(j, M) {
+            psum[i][j] += psum[i - 1][j];
+        }
+    }
+
+    FOR2(i, 0, N+1) {
+        FOR(j, M) {
+            dp[i][j] = INT_MAX;
+        }
+    }
+
+    fill(dpmin + 1, dpmin + N + 1, INT_MAX);
+
+    FOR2(i, 1, N+1) {
+        FOR(j, M) {
+            if(dp[i-1][j] != INT_MAX) {
+                dp[i][j] = min(dp[i][j], dp[i - 1][j] + cost[i - 1][j]);
+            }
+            if(i >= K && dpmin[i-K] != INT_MAX) {
+                dp[i][j] = min(dp[i][j], dpmin[i-K] + psum[i-1][j] - psum[i - K - 1][j]);
+            }
+
+            if(dp[i][j] != INT_MAX) {
+                dpmin[i] = min(dpmin[i], dp[i][j]);
+            }
+        }
+    }
+
+    cout << dpmin[N] << "\n";
+}
